e6697b7948902a75c44c69d2ed9f19acf98155a9
Bhavyasree Rallapalli <112577858+breerall@users.noreply.github.com>
2023-11-08 13:02:23 -0500

    Add files via upload

--- jsChallenges.js
+++ jsChallenges.js
@@ -1,56 +1,94 @@
-// Color of the Day: Coral <Cassidy Petersen>
+//color of the day: Coral - Bhavya
 
-
-// This is done by changing the HTML file
-// function challenge1() {
-//     //Fetch the vaule of the checkbox 
-//     // see if it is checked
-//     // change the email field accordingly
-
-//     let checkBox = document.getElementById("subscribe").checked;
-//     if (checkBox){ 
-//         document.querySelector("#emailDiv").style.display = "block";
-//     } else { 
+//Traditional Way of modifying HTML
+// function challenge1(){
+//     //Fetch the value of the Checkbox
+//     if (document.getElementById("subscribe").checked == true){
+//         //See if it is checked
+//         document.querySelector("#emailDiv").style.display = "initial";
+//         //Change the email field Accordingly
+//     } else {
 //         document.querySelector("#emailDiv").style.display = "none";
 //     }
 // }
 
-// By adding Event Listener
-// let varCheckbox = document.getElementById("subscribe");
 
-// varCheckbox.addEventListener("click",function() {
-//     if (varCheckbox.checked) {
-//         document.querySelector("#emailDiv").style.display = "block";
-//     } else {
-//         document.querySelector("#emailDiv").style.display = "none";
+// function challenge2(){
+//     if (document.getElementById("sameAddress").checked == true){
+//         console.log("this works")
+//         console.log(document.querySelector("#bill").textarea);
+//         document.querySelector("#home").value = document.querySelector("#bill").value;
 //     }
+//     else{
+//         document.querySelector("#home").value = prompt("Enter Billing Address");
+//     }
+// }
 
+// Challenge 1
+// let varCheckBox = document.getElementById("subscribe");
+// varCheckBox.addEventListener("click", function(){
+
+//     if(varCheckBox.checked ==true){
+//         document.querySelector("#emailDiv").style.display = "initial";
+//     } else{
+//         document.querySelector("#emailDiv").style.display = "none";   
+//     }
 // });
 
-// Challenge 2
-let SameAsAbove = document.getElementById("sameAddress");
 
-SameAsAbove.addEventListener("click",function() {
-    if (SameAsAbove.checked == true) {
-        document.querySelector("#home").value = document.querySelector("#bill").value;
-    } else {
-        document.querySelector("#home").value = prompt("Enter Billing Address");
-    }
+//Challenge 2
+// let varSameAsAbove = document.getElementById("sameAddress");
 
-});
+// varSameAsAbove.addEventListener("click", function(){
 
-// Challenge 3
-let SameAsAbove = document.getElementById("sameAddress");
+//     if(varSameAsAbove.checked == true){
+//         console.log("this works")
+//         document.querySelector("#home").value = document.querySelector("#bill").value;
+//     } else{
+//         document.querySelector("#home").value = prompt("Enter Billing Address");  
+//     }
+// });
 
-SameAsAbove.addEventListener("click",function() {
-    if (SameAsAbove.checked == true) {
-        document.querySelector("#home").value = document.querySelector("#bill").value;
-    } else {
-        document.querySelector("#home").value = prompt("Enter Billing Address");
-    }
-
-});
+// function challenge3(){
+// if (form.querySelector("input[type=radio]:checked"));
+// var value;
+// if (!checked) {
+//     alert("You need to select one of the options");
+// }
+// }
 
 
+//Challenge 3
+let box1Check = document.querySelectorAll('input[name="standing"]');
+let box2Check = document.querySelectorAll('input[name="gradDate"]');
+let submitButton = document.querySelector('input[type="submit"]');
+//get value of radio buttons
+//check if both radio buttons are selected
+
+submitButton.addEventListener("click", function() {
+
+     // Check if at least one radio button in the first group is checked
+     let box1Selected = Array.from(box1Check).some(input => input.checked);
+
+     // Check if at least one radio button in the second group is checked
+     let box2Selected = Array.from(box2Check).some(input => input.checked);
+ 
+     // Check if both groups have at least one radio button selected
+     if (!(box1Selected && box2Selected)) {
+         alert("Please select an option for both current class standing and anticipated graduation date.");
+     } else {
+         // If both selections are made, allow form submission
+         document.getElementById("ch3form").submit();
+     }
+ });
 
 
+// document.querySelectorAll('input[name="submit"]').addEventListener("click", function(){
+//     console.log(document.querySelectorAll('input[name="standing"]'));
+//     console.log(document.querySelectorAll('input[name="standing"]'));
+//     if (!(box1Check.checked == true && box2Check.checked == true)){
+//         alert("Please Enter both Values!")
+//     }
+// });
+//if selected pass
+//if not selected show alert
